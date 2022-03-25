f1b3c1f59f6f17ed7e100171c98ddcb9332a9440
Apoh <Edenra08@gmail.com>
2022-03-25 13:28:28 +0000

    Assignment complete

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+
+// describe("index.js", () => {
+//     it("has a function expression called divide", () => {
+//       expect(divide).to.exist
+//     })
+  
+
+
+const divide = num => 2000 / 100
+
+const square = num1 => 2 * 2;
+
+const add = (num2, num3) => 3 + 4 ;
+       
